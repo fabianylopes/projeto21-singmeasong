77ad667c7b0c1158b4bb0ea34706e3c6e3d1263f
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-07-30 10:25:57 -0300

    add reset db command

--- front-end/cypress/e2e/recommendations.cy.js
+++ front-end/cypress/e2e/recommendations.cy.js
@@ -2,21 +2,43 @@
 
 import { faker } from '@faker-js/faker';
 
-describe('recommendations suit test', () => {
-    it('should insert recommendation', () => {
+const URL = 'http://localhost:3000';
 
+describe('recommendations suit test', () => {  
+    beforeEach(() => {
+        cy.resetDb();
+    })
+    
+    it('should insert recommendation', () => {
         const body = {
             name: faker.music.songName(),
             url: 'https://www.youtube.com/watch?v=UCCyoocDxBA'
         }
         
-        cy.visit('http://localhost:3000/');
-        cy.get('input[placeholder="Name"]').type(body.name);
-        cy.get('input[placeholder="https://youtu.be/..."]').type(body.url);
+        cy.visit(`${URL}`);
+        cy.get('#name').type(body.name);
+        cy.get('#url').type(body.url);
 
-        cy.intercept('POST', 'http://localhost:3000/recommendations').as('insertRecommandation')
-        cy.get("button").click();
+        cy.intercept("POST", "/recommendations").as('insertRecommandation')
+        cy.get("#button").click();
         cy.wait("@insertRecommandation");
-        cy.contains(body.name);
+
+        cy.contains(body.name).end();
     });
+
+    it('with empry body should show error window', () => {
+        const body = {};
+
+        cy.visit(`${URL}`);
+        cy.get("#button").click();
+        
+    });
+
+    /* it('should upvote', () => {
+
+    });
+
+    it('should downvote', () => {
+        
+    }); */
 });
