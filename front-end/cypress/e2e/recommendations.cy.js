abcde6833c0078727ecf6cfcb966ac7c521a3781
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-07-31 13:33:26 -0300

    cypress tests

--- front-end/cypress/e2e/recommendations.cy.js
+++ front-end/cypress/e2e/recommendations.cy.js
@@ -5,40 +5,55 @@ import { faker } from '@faker-js/faker';
 const URL = 'http://localhost:3000';
 
 describe('recommendations suit test', () => {  
-    beforeEach(() => {
+    /* beforeEach(() => {
         cy.resetDb();
-    })
+    }) */
     
-    it('should insert recommendation', () => {
+   /*  it('should insert recommendation', () => {
         const body = {
             name: faker.music.songName(),
             url: 'https://www.youtube.com/watch?v=UCCyoocDxBA'
         }
         
         cy.visit(`${URL}`);
+  
         cy.get('#name').type(body.name);
         cy.get('#url').type(body.url);
-
+    
         cy.intercept("POST", "/recommendations").as('insertRecommandation')
         cy.get("#button").click();
         cy.wait("@insertRecommandation");
 
-        cy.contains(body.name).end();
-    });
+        cy.contains(body.name).should('be.visible');
 
-    it('with empry body should show error window', () => {
+        cy.end();
+    }); */
+
+    it('with empty body should show error window', () => {
         const body = {};
 
+        cy.insertRecommandation(body);
+
+        cy.on('window:alert', (text) => {
+            expect(text).to.contains('Error creating recommendation!').end();
+        });
+        
+    });
+
+    it('should upvote when button is clicked', () => {
+
         cy.visit(`${URL}`);
-        cy.get("#button").click();
-        
-    });
 
-    /* it('should upvote', () => {
+        cy.get('#arrowUp').click();
+
 
     });
 
-    it('should downvote', () => {
+    it('should downvote when button is clicked', () => {
         
-    }); */
+        cy.visit(`${URL}`);
+
+        cy.get('#arrowDown').click();
+
+    });
 });
--- front-end/cypress/support/commands.js
+++ front-end/cypress/support/commands.js
@@ -1,4 +1,14 @@
-Cypress.Commands.add("resetDb", () => {
-    cy.log("cleanning database");
-    cy.request("POST", "http://localhost:5000/reset", {});
+Cypress.Commands.add('insertRecommandation', (body) => {
+    cy.visit('http://localhost:3000/');
+  
+    cy.get('#name').type(body.name);
+    cy.get('#url').type(body.url);
+  
+    cy.intercept("POST", "/recommendations").as('insertRecommandation')
+    cy.get("#button").click();
+    cy.wait("@insertRecommandation");
 });
+
+Cypress.Commands.add('resetDb', () => {
+    cy.request('POST', "http://localhost:5000/reset", {});
+  });
